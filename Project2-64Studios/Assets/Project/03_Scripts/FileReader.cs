14e90a788a439ad95584c2312d56bcd98e90c804
Xasa7980 <javier.sanchez@enti.cat>
2025-10-05 19:27:42 +0200

    Finished level 3-4-5
    stills need mantaining and re-do for our gamePlay structure

--- Project2-64Studios/Assets/Project/03_Scripts/FileReader.cs
+++ Project2-64Studios/Assets/Project/03_Scripts/FileReader.cs
@@ -5,10 +5,13 @@ using System.Linq;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System;
+using UnityEngine.InputSystem.Controls;
+using System.Text.RegularExpressions;
+using UnityEngine.Rendering.Universal;
 
 public class FileReader : MonoBehaviour
 {
-    bool file_is_Open = false;
+    bool fileChanged = false;
     private string previousContent;
     [Serializable]
     public struct GameObjectReferencesDictionary
@@ -20,15 +23,22 @@ public class FileReader : MonoBehaviour
     FileSystemWatcher systemWatcher;
     string filePath;
     string[] lines;
+    [SerializeField] Light2D globalLight; //lvl3
+    /// <summary>
+    /// Nivel 5:
+    [SerializeField] GameObject redTilesPrefab;
+    [SerializeField] GameObject greenTilesPrefab;
+    [SerializeField] float tileSize = 1;
+    /// </summary>
     void Start()
     {
-        string persistentDir = Path.Combine(Application.persistentDataPath, "Level-3");
+        string persistentDir = Path.Combine(Application.persistentDataPath, "Level-5");
         if (!Directory.Exists(persistentDir))
         { 
                 Directory.CreateDirectory(persistentDir); 
         }
 
-        filePath = Path.Combine(Application.persistentDataPath, "Level-3/Puzzle.txt");
+        filePath = Path.Combine(Application.persistentDataPath, "Level-5/Puzzle.txt");
         systemWatcher = new FileSystemWatcher();
         systemWatcher.Path = Path.GetDirectoryName(filePath);
         systemWatcher.Filter = Path.GetFileName(filePath);
@@ -37,7 +47,7 @@ public class FileReader : MonoBehaviour
          
         if (!File.Exists(filePath))
         {
-            string sourceFile = Path.Combine(Application.streamingAssetsPath, "Level-3/Puzzle.txt");
+            string sourceFile = Path.Combine(Application.streamingAssetsPath, "Level-5/Puzzle.txt");
             if (File.Exists(sourceFile))
             {
                 File.Copy(sourceFile, filePath);
@@ -52,37 +62,38 @@ public class FileReader : MonoBehaviour
         previousContent = File.ReadAllText(filePath);
         systemWatcher.Changed += OnFileChanged;
     }
-
-    void Update()
+    void OnFileChanged ( object source, FileSystemEventArgs e )
     {
-        Level3(filePath);
+        UnityEngine.Debug.Log("El archivo fue modificado " + e.FullPath);
+        fileChanged = true;
     }
-    void Level3(string fileName)
+
+    void Update ()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (File.Exists(filePath))
             {
                 Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true }); //Abrir ventana
-                file_is_Open = true;
             }
             else
             {
                 UnityEngine.Debug.LogError("No se encontró el archivo: " + filePath);
             }
-            //ReadTextFile(filePath);
+        }
+        //Level3(filePath);
+        //Level4(filePath);
+        Level5(filePath);
+    }
+    void Level3(string fileName)
+    {
+
+        if (fileChanged)
+        {
+            fileChanged = false;
+            ReadTextFile(filePath);
         }
 
-        //if (file_is_Open)
-        //{
-        //    ReadTextFile(filePath);
-        //}
-
-    }
-    void OnFileChanged(object source, FileSystemEventArgs e )
-    {
-        UnityEngine.Debug.Log("El archivo fue modificado " + e.FullPath);
-        ReadTextFile(filePath);
     }
     void ReadTextFile(string fileName)
     {
@@ -98,14 +109,10 @@ public class FileReader : MonoBehaviour
             }
             string[] asteriscos = line.Trim().Split('*');
             keyIndex += asteriscos.Length - 1;
-            print(keyIndex);
         }
-        if (keyIndex > gameObjectReferencedInText.Count - 1)
-        {
-            VanishElement(gameObjectReferencedInText.Count - 1);
-        }
-        print("he llegado aqui");
+        print(keyIndex);
         VanishElement(keyIndex);
+        print("he llegado aqui");
     }
     void VanishElement ( int index )
     {
@@ -115,7 +122,7 @@ public class FileReader : MonoBehaviour
             return;
         }
 
-        var go = gameObjectReferencedInText[index].value;
+        GameObject go = gameObjectReferencedInText[index].value;
 
         if (go == null)
         {
@@ -125,6 +132,104 @@ public class FileReader : MonoBehaviour
 
         UnityEngine.Debug.Log($" Desactivando: {go.name}");
         go.SetActive(false);
+    }
+    void Level4 ( string fileName )
+    {
+        string[] lines = File.ReadAllLines(fileName);
+
+        if (lines.Length == 0)
+        {
+            UnityEngine.Debug.LogWarning("El archivo está vacío.");
+            return;
+        }
+
+        float valueFound = -1f;
+
+        foreach (string line in lines)
+        {
+
+            Match match = Regex.Match(line, @"[-+]?[0-9]*\.?[0-9]+"); 
+            //Sirve para buscar el primer numero entero o decimal negativo o positivo
+
+            if (match.Success)
+            {
+                // Convertir el texto del número en float
+                if (float.TryParse(match.Value, System.Globalization.NumberStyles.Float,
+                                    System.Globalization.CultureInfo.InvariantCulture, out float val))
+                {
+                    valueFound = val;
+                    UnityEngine.Debug.Log($"Valor numérico encontrado: {valueFound}");
+                    break;
+                }
+            }
+        }
+
+        if (valueFound >= 0)
+        {
+            globalLight.intensity = valueFound;
+            UnityEngine.Debug.Log($"Intensidad actualizada a: {valueFound}");
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("No se encontró ningún valor numérico válido en el archivo.");
+        }
+        valueFound = Mathf.Clamp(valueFound, 0f, 360f);
+        globalLight.pointLightInnerAngle = valueFound;
+    }
+    List<GameObject> instantiatedPrefabs = new List<GameObject>();
+    void Level5 ( string fileName )
+    {
+        if (fileChanged)
+        {
+            fileChanged = false;
+            string[] lines = File.ReadAllLines(fileName);
+
+            if (lines.Length == 0)
+            {
+                UnityEngine.Debug.LogWarning("El archivo está vacío.");
+                return;
+            }
+            int x = 0;
+            int y = 0;
+
+            if (instantiatedPrefabs.Count > 0)
+            {
+                for (int i = 0; i < instantiatedPrefabs.Count; i++)
+                {
+                    DestroyImmediate(instantiatedPrefabs[i]);
+                }
+                instantiatedPrefabs.Clear();
+                instantiatedPrefabs = new List<GameObject>();
+            }
+            foreach (string line in lines)
+            {
+
+                foreach(char c in line)
+                {
+                    Vector3 topLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height, Camera.main.nearClipPlane));
+                    Vector3 position = topLeft + new Vector3(0.5f + x * tileSize,- 0.5f + -y * tileSize, 0); x++;
+                    GameObject prefabToUse = null;
+                    switch (c)
+                    {
+                        case 'R':
+                            prefabToUse = redTilesPrefab;
+                            break;
+                        case 'G':
+                            prefabToUse = greenTilesPrefab;
+                            break ;
+                        default:
+                            prefabToUse = redTilesPrefab;
+                            break;
+                    }
+                    if(prefabToUse != null)
+                    {
+                        instantiatedPrefabs.Add(Instantiate (prefabToUse, position, Quaternion.identity,transform));
+                    }
+                }
+                x= 0;
+                y++;
+            } 
+        }
     }
 } 
 /*
